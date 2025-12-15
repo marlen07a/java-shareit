2d6c9f6261e6e2b00dba825ee8e9d36ad83562b8
marlen07a <marlen.amangeldi.06@gmail.com>
2025-12-16 01:25:04 +0500

    Implement User and Item CRUD with in-memory storage, validation, and exception handling, plus Booking and Request entity skeletons

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,10 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * TODO Sprint add-bookings.
- */
 @RestController
-@RequestMapping(path = "/bookings")
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
 public class BookingController {
 }
---
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.user;
+
+public class UserMapper {
+    public static UserDto toUserDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail()
+        );
+    }
+
+    public static User toUser(UserDto userDto) {
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        return user;
+    }
+}
