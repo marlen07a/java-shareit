3829f6e5873a5db0eb17f67d901043f94ae691ed
marlen07a <marlen.amangeldi.06@gmail.com>
2025-12-23 00:06:36 +0500

    add booking and commenting opportunities

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -1,16 +1,17 @@
 package ru.practicum.shareit.item;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface ItemRepository {
-    Item save(Item item);
-
-    Optional<Item> findById(Long id);
-
+public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerId(Long ownerId);
 
+    @Query("select i from Item i " +
+            "where i.available = true and " +
+            "(lower(i.name) like lower(concat('%', ?1, '%')) " +
+            " or lower(i.description) like lower(concat('%', ?1, '%')))")
     List<Item> search(String text);
 }
--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.NotFoundException;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
     private final UserRepository repository;
 
@@ -27,21 +29,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public UserDto createUser(UserDto userDto) {
-        if (!repository.isEmailUnique(userDto.getEmail(), null)) {
-            throw new RuntimeException("Email already exists");
-        }
         User user = UserMapper.toUser(userDto);
         return UserMapper.toUserDto(repository.save(user));
     }
 
+    @Override
+    @Transactional
     public UserDto updateUser(Long id, UserDto userDto) {
         User user = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException("User not found"));
-
-        if (userDto.getEmail() != null && !repository.isEmailUnique(userDto.getEmail(), id)) {
-            throw new RuntimeException("Email already exists");
-        }
 
         if (userDto.getName() != null) user.setName(userDto.getName());
         if (userDto.getEmail() != null) user.setEmail(userDto.getEmail());
@@ -50,6 +48,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         repository.deleteById(id);
     }
