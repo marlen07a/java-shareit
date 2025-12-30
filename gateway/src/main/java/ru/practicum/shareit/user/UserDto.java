1a1796a622a2869c3b7616f9a0faae4af16376ba
Marlen <marlen.amangeldi.06@gmail.com>
2025-12-30 12:06:45 +0500

    removed unused imports

--- gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
